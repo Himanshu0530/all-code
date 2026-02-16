c7a40718a9fbd9c5ccfc166cf7c74c1b9f1ff6c3
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-02-17 00:20:34 +0530

    Initial commit

---
+++ swap3v.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class swap3v {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // swapping number using 3rd variable
+        int c = a;
+        a = b;
+        b = c;
+
+    
+
+    System.out.println(a);
+
+    System.out.println(b);
+    }
+
+}
