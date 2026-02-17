92160052c75803a2da5e1734e3c070f1528812af
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-02-18 00:32:27 +0530

    initial commit

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class table {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int n= sc.nextInt();
+        int sum;
+        for(i=1; i<=10; i++){
+            sum=i*n;
+            System.out.println(sum );
+            sc.close();
+        }
+    }
+}
+    
