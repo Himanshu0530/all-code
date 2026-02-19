c75b027b11051672364d9aec2413200b7b713c32
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-02-20 01:06:17 +0530

    even sum

---
+++ sumeven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sumeven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int n=sc.nextInt();
+        int sum =0;
+        for(i=1; i<=n; i++){
+            if(i%2==0)
+                sum = sum+i;
+            System.out.println("sum of even " +  sum);
+            sc.close();
+                
+        }
+    }
+    
+}
