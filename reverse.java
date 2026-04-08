ac865f16d0bae5de83fff1c8bf52c4b8e4a870cf
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-04-09 01:19:25 +0530

    2d array

---
+++ reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class mar30 {
+    public static void main(String[]args){
+        int [][] array = {{1,2,3},{4,5,6},{6,7,8}};
+        int i = 0;
+        int j = array.length-1;
+        while(i<j){
+            int [] temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+        for(int [] a : array){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
