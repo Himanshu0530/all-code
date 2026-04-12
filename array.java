364b0bc7013d5210e7f48422c55ecb6cbaab02fa
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-04-13 02:04:12 +0530

    3d array

---
+++ array.java
@@ -0,0 +1,31 @@
+public class array {
+    public static void main(String[] args) {
+        int [] [] flats;
+        flats = new int [3] [3];
+        flats [0][0] = 001;
+        flats [0][1] = 002;
+        flats [0][2] = 003;
+
+        flats [1][0] = 101;
+        flats [1][1] = 102;
+        flats [1][2] = 103;
+
+        flats [2][0] = 201;
+        flats [2][1] = 202;
+        flats [2][2] = 203;
+
+        for(int i =0; i<flats.length;i++){
+            for(int j = 0; j<flats[i].length;j++){
+                System.out.print(flats[i][j] + "  ");
+               
+            }
+        
+        System.out.println(" ");
+        }
+
+    }
+
+}
+    
+    
+
